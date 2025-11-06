2c35679b0458fc0bc3fbe79e4f5cd87c40d8e09f
Joao <232938876+joaokhalaf@users.noreply.github.com>
2025-11-05 21:56:08 -0300

    feat: not found page and add SEO (sitemap & robots)

---
+++ app/sitemap.ts
@@ -0,0 +1,11 @@
+export const baseUrl = 'https://joaokhalaf.dev';
+
+export default async function sitemap() {
+
+  let routes = ['', ].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }))
+
+  return [...routes]
+}
